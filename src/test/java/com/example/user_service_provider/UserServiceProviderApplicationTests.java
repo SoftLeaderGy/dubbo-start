247c7ac525d7537871837a1aa6e820966fb13b65
SoftLeaderGy <826044698@qq.com>
2022-09-22 19:20:38 +0800

    Initial commit

---
+++ src/test/java/com/example/user_service_provider/UserServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_service_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
